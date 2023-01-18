316b59cc5c63c25e3b53bd8120a43d69d8746187
egro <profissional.eliasgro@gmail.com>
2023-01-18 00:15:09 -0300

    aula 1 upload 1.2

--- src/web/tailwind.config.cjs
+++ src/web/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.tsx",
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09098A'
+      },
+    },
   },
   plugins: [],
 }
